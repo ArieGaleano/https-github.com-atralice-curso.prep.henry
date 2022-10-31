3e34fa441cdc09fd2e8a2c0dec29f62f374eb05b
ArielGaleano <g7807710@gmail.com>
2022-10-31 17:43:26 -0600

    prueba

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "Hola";
+const nuevaString = "prueba";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
@@ -13,11 +13,10 @@ const nuevoBool = null;
 const nuevaResta = 10 - null === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * null === 40;
 
 // Resuelve el siguiente problema matemático:
 const nuevoModulo = 21 % 5 === null;
-
 
 // En los próximos 22 problemas, deberás completar la función.
 // Todo tu código irá dentro de las llaves de la función.
@@ -140,7 +139,6 @@ function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un cuadrado teniendo su altura y ancho
   // Tu código:
 }
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
